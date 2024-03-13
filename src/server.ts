2111d7e332c88261bb0862b1e595e9c22949a6f8
Rodrigo Carvalho <rodrigo010carvalho@gmail.com>
2024-03-13 12:25:30 -0300

    adicionada rota para criação de filmes

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,42 @@ const port = 3000
 const app = express()
 const prisma = new PrismaClient();
 
+app.use(express.json())
+
 app.get("/movies", async (req,res) => {
-    const movies = await prisma.movie.findMany();
+    const movies = await prisma.movie.findMany({
+        orderBy: {
+            title: 'asc'
+        },
+        include: {
+            genres: true,
+            languages: true
+        }
+    });
     res.json(movies)
 });
+
+app.post('/movies', async (req, res) => {
+    console.log(`Conteúdo do body enviado na requisição: ${req.body.title}`);
+
+    const { title, genre_id, language_id, oscar_count, release_date } = req.body
+
+    try{
+
+    await prisma.movie.create({
+        data: {
+            title,
+            genre_id,
+            language_id,
+            oscar_count,
+            release_date: new Date(release_date)
+        }
+    });
+}catch(err){
+    return res.status(500).send({message:"Falha ao cadastrar um filme."})
+}
+    res.status(201).send()    
+})
 
 app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`);
